3ce27992c3149a9f18bd6eeea96aae23d05129c4
hoanghao01 <haonguyen6345@gmail.com>
2024-04-24 23:56:19 +0700

    add database config

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,12 +5,12 @@ const router = express.Router();
 const controller = require('../controllers/indexController');
 
 //routes
-// router.get('/createTables', (req, res) => {
-//     let models = require(.'./models');
-//     models.sequelize.sync().then(() => {   
-//         res.send('Tables are created!');
-//     });
-// });
+router.get('/createTables', (req, res) => {
+    let models = require('../models');
+    models.sequelize.sync().then(() => {   
+        res.send('Tables are created!');
+    });
+});
 router.get('/', controller.showHomePage);
 
 router.get('/:page', controller.showPage);
