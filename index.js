b885c473d3480701638567b9d4cdfc1b77706b43
hoanghao01 <haonguyen6345@gmail.com>
2024-04-25 21:54:47 +0700

    using dotenv

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //express is a node module for building web server
 'use strict'
+require('dotenv').config(); //load bien moi truong tu file .env
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,8 +12,9 @@ const session = require('express-session');
 const redisStore = require('connect-redis').default;
 const { createClient } = require('redis');
 const redisClient = createClient({
-    //url: 'rediss://red-col6j4q0si5c73e701sg:5O3SlxhtD4WL2yHm4HLa5V11OqtUzXeh@singapore-redis.render.com:6379'
-    url: 'redis://red-col6j4q0si5c73e701sg:6379'
+    //url: 'rediss://red-col6j4q0si5c73e701sg:5O3SlxhtD4WL2yHm4HLa5V11OqtUzXeh@singapore-redis.render.com:6379' //localhost
+    //url: 'redis://red-col6j4q0si5c73e701sg:6379'    //url cua redis tren onrender
+    url: process.env.REDIS_URL
 });
 redisClient.connect().catch(console.error);
 
@@ -40,7 +43,7 @@ app.use(express.urlencoded({ extended: false }));
 
 //cau hinh session - cai dat session: pnpm install express-session || pnpm i -s express-session
 app.use(session({
-    secret: 'S3cret', //chuoi bi mat de ma hoa session id
+    secret: process.env.SESSION_SECRET, //chuoi bi mat de ma hoa session id
     store: new redisStore({ client: redisClient }),  //luu session vao redis 
     resave: false,  //session chi duoc luu lai khi co su thay doi 
     saveUninitialized: false,   //session duoc tao ra ngay lap tuc ma khong can su kien gi xay ra 
